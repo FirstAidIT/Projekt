0dc441554290c770cdf7f971fdd74d1182eacf4d
ginderella7 <43462798+ginderella7@users.noreply.github.com>
2020-01-20 14:46:37 +0100

    Update db_connection.php

--- src/datenbank/db_connection.php
+++ src/datenbank/db_connection.php
@@ -1,5 +1,5 @@
 <?php
-$db = new PDO ('mysql:host=141.13.8.16; port=3307;dbname=wip19_g3','wip19_g3', 'A!2dUMx');
+$db = new PDO ('mysql:host=; port=;dbname=','', '');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
 
