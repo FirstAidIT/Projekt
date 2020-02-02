1c189d2e0bc2edee5e0627a9bbc06bdb5f73ca5c
Alex Werner <alex@familie-werner-privat.de>
2020-02-02 14:26:26 +0100

    ..

--- src/einzelprojekt.php
+++ src/einzelprojekt.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
     <link rel="stylesheet" href="/main.css">
 <title>Einzelprojektansicht</title>
-<!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 </head>
 <body>
