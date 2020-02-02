1207895df3a8c44da199b8f23490697b0b70dd71
MrBroetchen <stefan.grusss@gmail.com>
2020-02-02 15:04:23 +0100

    tre Signed-off-by: MrBroetchen <stefan.grusss@gmail.com>

--- src/Dashboard/mitarbeiterdashboard.php
+++ src/Dashboard/mitarbeiterdashboard.php
@@ -4,12 +4,12 @@
 <head>
 <meta charset="utf-8">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/mitarbeiterdashboard.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 <title>Mitarbeiterdashboard</title>
 <!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/mitarbeiterdashboard.css">
 
 
 </head>
