8047b2a6abf9ba16a23385542580340ca3be12fd
Alex Werner <alex@familie-werner-privat.de>
2020-02-02 15:52:45 +0100

    ..

--- src/Logfiles/functions.js
+++ src/Logfiles/functions.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 	$("#skill").on("show.bs.modal", function(event){
 		// Place the returned HTML into the selected element
-		$(this).find(".modal-body-skill").load("/skillaenderung.php");
+		$(this).find(".modal-skill").load("/skillaenderung.php");
 	});
 	
 	$("#project").on("show.bs.modal", function(event){
 		// Place the returned HTML into the selected element
-		$(this).find(".modal-body-project").load("/projects.php");
+		$(this).find(".modal-project").load("/projects.php");
 	});
 });
